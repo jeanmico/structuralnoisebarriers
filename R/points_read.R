258ee685ba3528d235bae2e5558d4940b2e6ebd1
jeanmico <costellojeanm@gmail.com>
2025-05-28 18:05:29 -0400

    add sf dependency

--- R/points_read.R
+++ R/points_read.R
@@ -1,3 +1,11 @@
+#' Read user-supplied file of points
+#'
+#' @param points_file path to an sf object file
+#'
+#' @return a dataframe
+#' @export
+#'
+#' @examples
 points_read <- function(points_file) {
-  read.csv(points_file)
+  sf::read_sf(points_file)
 }
